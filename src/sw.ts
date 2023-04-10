81d1bebbb7b419fcd11603386a45ee2b7d55786b
Sam Noedel <sam.noedel@gmail.com>
2023-04-09 22:49:22 -0700

    Update old cache stuff

--- src/sw.ts
+++ src/sw.ts
@@ -1,34 +1,34 @@
-const CACHE_NAME = `baby-shift-0.1.0`;
+const CACHE_NAME = `baby-shift-0.2.0`
 
-self.addEventListener("install", (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
     (async () => {
-      let cache = await caches.open(CACHE_NAME);
-      await cache.addAll(["/", "/script.js"]);
+      let cache = await caches.open(CACHE_NAME)
+      await cache.addAll(['/', '/index.js', '/styles.css', '/index.css'])
     })()
-  );
-});
+  )
+})
 
-self.addEventListener("activate", (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
     (async () => {
-      let keyList = await caches.keys();
+      let keyList = await caches.keys()
       return Promise.all(
-        keyList.map((key) => {
-          if (key === CACHE_NAME) return Promise.resolve();
-          return caches.delete(key);
+        keyList.map(key => {
+          if (key === CACHE_NAME) return Promise.resolve()
+          return caches.delete(key)
         })
-      );
+      )
     })()
-  );
-});
+  )
+})
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     (async () => {
-      let cachedResponse = await caches.match(event.request);
-      if (cachedResponse) return cachedResponse;
-      return fetch(event.request);
+      let cachedResponse = await caches.match(event.request)
+      if (cachedResponse) return cachedResponse
+      return fetch(event.request)
     })()
-  );
-});
+  )
+})
