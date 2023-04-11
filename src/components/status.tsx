c44e0f138f62850c6275b9a070e2bb73965867b7
Sam Noedel <sam.noedel@gmail.com>
2023-04-11 14:05:08 -0700

    Move global styles into CSS modules & delete separate stylesheet

--- src/components/status.tsx
+++ src/components/status.tsx
@@ -1,17 +1,9 @@
-import { History } from '@components'
 import { useGetHistoryState } from '@clients'
-import { getIsAsleep, getMostRecentLog } from '@store'
+import { getIsAsleep } from '@store'
 import styles from './status.module.scss'
-
-let bottleSum = (history: History | null): string => {
-  if (!history?.bottles) return '--'
-  let sum = history.bottles.reduce((sum, x) => sum + x)
-  return `${sum}oz`
-}
 
 export let Status = () => {
   let { data: history = [] } = useGetHistoryState(undefined)
-  let latest = getMostRecentLog(history)
   let isAsleep = getIsAsleep(history)
   let status = isAsleep ? ('asleep' as const) : ('awake' as const)
   let emoji = isAsleep ? '😴' : '👶'
